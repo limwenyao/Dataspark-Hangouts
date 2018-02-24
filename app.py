fbe194237bce2379fa1b32c94f4584cc4f94d725
Yilong94 <yilong_tan@mymail.sutd.edu.sg>
2018-02-25 03:30:35 +0800

    Added key's sentiment analysis

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, render_template
 from getAllEvents import getAllEvents
 from datetime import datetime
+from getSentiments import Sentiment
 
 app = Flask(__name__)
 
@@ -8,15 +9,29 @@ app = Flask(__name__)
 def show_viz():
 
 	# placeholder for event title, event information, event date and event coordinates
-	
+
 	# hardcoded date for hackathon purposes
 	startDate = datetime(2018, 2, 17)
 	endDate = datetime(2018, 2, 18)
+
+	# event -> [{eventName:?, eventInfo:?, eventDate:?, eventCoord:?}, ...]
 	event = getAllEvents.getAllEvents(startDate, endDate)
-	# print event
+	print event
+
+
 
 	# placeholder for aggregated data for heatmap in json format
 
+
+
+
 	# placeholder for sentiment analysis
+	# listOfSentiment -> [([(tweet1,polarity1), (tweet2,polarity2),(tweet3,polarity3)],0.66), ....]
+	listOfSentiment = []
+	for eachEvent in event:
+		listOfSentiment.append(Sentiment.getSentiment(eachEvent['eventName']))
+	print listOfSentiment
+
+
 
 	return render_template('index.html', event=event, aggregated=None, sentiment=None)
